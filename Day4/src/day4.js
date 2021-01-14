1d55a8b042c95f1133a1eeafc1cbe6a97e0d1086
ItsmeLee <arsmagica90@gmail.com>
2021-01-14 12:02:58 +0900

    Add resize event
    Add new resize event listener.
    When user resize the window size, the h1 tag color change to white.

--- Day4/src/day4.js
+++ Day4/src/day4.js
@@ -2,3 +2,16 @@
 //import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
+const h1 = document.querySelector('h1');
+const body = document.querySelector('body');
+
+function handleResize(){
+    h1.style.color = "white";
+    
+}
+
+function init(){
+    window.addEventListener('resize',handleResize);
+}
+
+init();
