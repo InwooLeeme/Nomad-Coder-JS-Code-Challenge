6711d5bf7388ba7b90e4c7d0a8b6b0c8656c7ea4
ItsmeLee <arsmagica90@gmail.com>
2021-01-15 23:10:23 +0900

    Complete christmas clock
    Complete time until christmas clock.
    If the day, hour, minute and seconds smaller than 10, add 0 automatically in front of each times.

--- Day5/src/day5.js
+++ Day5/src/day5.js
@@ -7,6 +7,12 @@ const ONE_DAY_MILLISECONDS = ONE_HOURS_MILLISECONDS * 24;
 const ONE_MINUTE_MILLISECONDS = ONE_HOURS_MILLISECONDS / 60;
 const ONE_SECOND_MILLISECONDS = 1000;
 
+const body = document.querySelector('body');
+const dayController = document.querySelector('.controllDay');
+const hourController = document.querySelector('.controllHour');
+const minController = document.querySelector('.controllMin');
+const secController = document.querySelector('.controllSec');
+
 function getTime() {
   // Don't delete this.
   const xmasDay = new Date("2021-12-24:00:00:00+0900");
@@ -15,9 +21,14 @@ function getTime() {
   const leftHours = Math.floor((leftMilliSeconds % ONE_DAY_MILLISECONDS) / ONE_HOURS_MILLISECONDS);
   const leftMinutes = Math.floor(((leftMilliSeconds % ONE_DAY_MILLISECONDS) % ONE_HOURS_MILLISECONDS) / ONE_MINUTE_MILLISECONDS);
   const leftSeconds = Math.floor((((leftMilliSeconds % ONE_DAY_MILLISECONDS) % ONE_HOURS_MILLISECONDS) % ONE_MINUTE_MILLISECONDS) / ONE_SECOND_MILLISECONDS);
+
+  dayController.innerHTML = `${leftDays < 10 ? `0${leftDays}` : `${leftDays}`}D`;
+  hourController.innerHTML = `${leftHours < 10 ? `0${leftHours}` : `${leftHours}`}H`;
+  minController.innerHTML = `${leftMinutes < 10 ? `0${leftMinutes}` : `${leftMinutes}`}M`;
+  secController.innerHTML = `${leftSeconds < 10 ? `0${leftSeconds}` : `${leftSeconds}`}S`;
 }
 
 function init() {
-  //setInterval(getTime, 1000);
+  setInterval(getTime, 1000);
 }
 init();
