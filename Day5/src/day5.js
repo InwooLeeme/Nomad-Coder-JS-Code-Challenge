285cf16726e75012073fb3000ee627aaacd47677
ItsmeLee <arsmagica90@gmail.com>
2021-01-15 22:23:52 +0900

    Get left days until christmas
    Added the function to bring back the days and milliseconds left by Christmas.

--- Day5/src/day5.js
+++ Day5/src/day5.js
@@ -2,11 +2,18 @@
 
 // You're gonna need this
 const NINE_HOURS_MILLISECONDS = 32400000;
+const ONE_HOURS_MILLISECONDS = 32400000 / 9;
+const ONE_DAY_MILLISECONDS = ONE_HOURS_MILLISECONDS * 24;
 
 function getTime() {
   // Don't delete this.
-  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const xmasDay = new Date("2021-12-24:00:00:00+0900");
+  const leftDayMilliSeconds = xmasDay - new Date();
+  const leftDays = Math.floor(leftDayMilliSeconds / ONE_DAY_MILLISECONDS);
+  
 }
 
-function init() {}
+function init() {
+  setInterval(getTime, 1000);
+}
 init();
