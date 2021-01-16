95e53a8aa484b9c98019720053a0890f63571bfc
ItsmeLee <arsmagica90@gmail.com>
2021-01-16 15:02:20 +0900

    Get country name from select
    Add value in each option.
    In day6.js file, add new function that get country name in each options.

--- Day6/src/day6.js
+++ Day6/src/day6.js
@@ -1,3 +1,17 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 //import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const body = document.querySelector("body");
+const selectBar = document.querySelector("select");
+
+function getCountryName(){
+    const currentName = selectBar.value;
+    console.log(currentName);
+}
+
+function init(){
+    selectBar.addEventListener('change', getCountryName);
+}
+
+init();
