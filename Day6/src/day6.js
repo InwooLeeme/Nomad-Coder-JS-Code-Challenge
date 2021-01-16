a8acb849af6aea8405f4e2d1c9228c5ad46a243c
ItsmeLee <arsmagica90@gmail.com>
2021-01-16 15:46:41 +0900

    Add Greece background and gradient
    When user select greece in select bar,
    the background image changes to a temple representing Greece and the Gradient of text changes to the color of the Greece flag.

--- Day6/src/day6.js
+++ Day6/src/day6.js
@@ -19,13 +19,22 @@ function changeBack(text){
     switch (text){
         case 'KR':
             selectKr();
-        break;
+            break;
+        case 'GE':
+            selectGE();
+            break;
     }
 }
 
 function selectKr(){
-    body.style.backgroundImage = `url(${"https://images.unsplash.com/photo-1485186667901-c039c19ecac3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTUyfHxrb3JlYXxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60"})`;
+    body.style.backgroundImage = `url(https://images.unsplash.com/photo-1485186667901-c039c19ecac3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTUyfHxrb3JlYXxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60)`;
     pgTitle.style.background = `linear-gradient(to right bottom, #be0818, #bd003b, #ac005f, #850085, #2307a6)`;
+    neverUsed();
+}
+
+function selectGE(){
+    body.style.backgroundImage = `url(https://images.unsplash.com/photo-1531816458010-fb7685eecbcb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)`;
+    pgTitle.style.background = `linear-gradient(to bottom, #2f17c9, #6f56db, #a08eeb, #cfc6f6, #ffffff)`;
     neverUsed();
 }
 
