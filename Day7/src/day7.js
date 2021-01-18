12ea53e0162b48e3f98740db78b8eabae35de104
ItsmeLee <arsmagica90@gmail.com>
2021-01-18 13:06:15 +0900

    Get input value from input box
    When user typing in input box, the handleSubmit function get input value from input box.

--- Day7/src/day7.js
+++ Day7/src/day7.js
@@ -3,12 +3,14 @@
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 const form = document.querySelector('.jsForm');
+const input = form.querySelector('input');
 
 //console.log(form);
 
 function handleSubmit(event){
     event.preventDefault();
-    
+    const currentText = input.value;
+    input.value = "";
 }
 
 function init(){
