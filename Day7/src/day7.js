172c2094d319d3f4457cba328712371f98b60761
ItsmeLee <arsmagica90@gmail.com>
2021-01-18 13:40:54 +0900

    Add list in pending ul
    Add the function that add input value in pending ul element.
    When user typing in input box and submit the text, the text add immediately under the pending ul.

--- Day7/src/day7.js
+++ Day7/src/day7.js
@@ -1,17 +1,44 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 //import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
-
+const body = document.querySelector('body');
 const form = document.querySelector('.jsForm');
 const input = form.querySelector('input');
 
-//console.log(form);
+let pendingArray = [];
 
 function handleSubmit(event){
     event.preventDefault();
     const currentText = input.value;
+    addPening(currentText);
     input.value = "";
 }
+
+function addPening(text){
+    const pendingUl = document.querySelector(".pendingList");
+    const li = document.createElement('li');
+    const planSave = document.createElement('span');
+    const checkSpan = document.createElement('span');
+    const deleteSpan = document.createElement('span');
+    checkSpan.innerHTML = `<i class="fas fa-check"></i>`;
+    deleteSpan.innerHTML = `<i class="fas fa-times"></i>`;
+    //span.addEventListener('click',handleClick);
+    planSave.innerHTML = text;
+    const pendingObj = {
+        id : pendingArray.length + 1,
+        text : text,
+    }
+    li.id = pendingObj.id;
+    pendingArray.push(pendingObj);
+    pendingUl.appendChild(li);
+    li.appendChild(deleteSpan);
+    li.appendChild(checkSpan);
+    li.appendChild(planSave);
+}
+/* 
+function handleClick(event){
+    
+} */
 
 function init(){
     form.addEventListener('submit',handleSubmit);
