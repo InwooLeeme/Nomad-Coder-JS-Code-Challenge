6b9b288982cf9ec6b3c58ebfdbeed7f3454c5b3b
ItsmeLee <arsmagica90@gmail.com>
2021-01-18 13:03:28 +0900

    prevent submit event
    Prevent submit event function in day7.js

--- Day7/src/day7.js
+++ Day7/src/day7.js
@@ -1,3 +1,18 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 //import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const form = document.querySelector('.jsForm');
+
+//console.log(form);
+
+function handleSubmit(event){
+    event.preventDefault();
+    
+}
+
+function init(){
+    form.addEventListener('submit',handleSubmit);
+}
+
+init();
