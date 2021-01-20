0222dd0c3769fa42e92a501427e3eaa68a9f1af4
ItsmeLee <arsmagica90@gmail.com>
2021-01-20 23:12:47 +0900

    Get range value function
    Add the new function that get current range value from range bar.

--- Day10/src/day10.js
+++ Day10/src/day10.js
@@ -1,3 +1,16 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 //import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const rangeBar = document.querySelector('.setRange');
+
+function handleRange(){
+    const currentRange = rangeBar.value;
+    console.log(currentRange);
+}
+
+function init(){
+    rangeBar.addEventListener('input', handleRange);
+}
+
+init();
