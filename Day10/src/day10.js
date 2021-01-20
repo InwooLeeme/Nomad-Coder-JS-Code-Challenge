3305f4d515955e509fc5048db3c3338262dd135a
ItsmeLee <arsmagica90@gmail.com>
2021-01-20 23:57:24 +0900

    Refactoring and play function
    Some refactoring in displayRange and genRandomNumber.
    Add the click event in play button.
    When user set the random number range, type in input box and click the play button, the game will be start.

--- Day10/src/day10.js
+++ Day10/src/day10.js
@@ -3,31 +3,37 @@
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 const rangeBar = document.querySelector('.setRange');
-const displayRange = document.getElementById("changeText");
+const displayRangeText = document.getElementById("changeText");
 const guessNumber = document.getElementById("guessNumber");
+const playBtn = document.getElementById("playBtn");
 
-function genRandomNumber(number){
-    const randNumber = Math.floor(Math.random() * number);
-    console.log(randNumber);
-}
-
-function handleRange(){
+function genRandomNumber(){
     const currentRange = rangeBar.value;
-    displayRange.innerText = currentRange;
-    genRandomNumber(currentRange);
+    const randNumber = Math.floor(Math.random() * currentRange);
+    return randNumber;
 }
-
 
 function getUserGuess(){
     const currentGuess = guessNumber.value;
     return currentGuess;
 }
 
+function displayRange(){
+    displayRangeText.innerText = rangeBar.value;
+}
 
+
+function inspectNumber(){
+    const guessNumber = parseInt(getUserGuess());
+    const randomNumber = genRandomNumber();
+    console.log(guessNumber, randomNumber);
+}
 
 function init(){
-    rangeBar.addEventListener('input', handleRange);
+    rangeBar.addEventListener('input', displayRange);
+    rangeBar.addEventListener('change', genRandomNumber);
     guessNumber.addEventListener('change', getUserGuess);
+    playBtn.addEventListener('click', inspectNumber);
 }
 
 init();
