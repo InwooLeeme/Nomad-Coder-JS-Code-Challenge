153aaf13ec7c25c1ce2090ccf7c011729fc791e4
ItsmeLee <arsmagica90@gmail.com>
2021-01-20 23:29:45 +0900

    Get user guess number and random number
    Add two function.
    One of the two function is get guess number from the user and the other function is generate random number when user set the number range.

--- Day10/src/day10.js
+++ Day10/src/day10.js
@@ -4,14 +4,30 @@
 
 const rangeBar = document.querySelector('.setRange');
 const displayRange = document.getElementById("changeText");
+const guessNumber = document.getElementById("guessNumber");
+
+function genRandomNumber(number){
+    const randNumber = Math.floor(Math.random() * number);
+    console.log(randNumber);
+}
 
 function handleRange(){
     const currentRange = rangeBar.value;
-    displayRange.innerText = currentRange;    
+    displayRange.innerText = currentRange;
+    genRandomNumber(currentRange);
 }
+
+
+function getUserGuess(){
+    const currentGuess = guessNumber.value;
+    return currentGuess;
+}
+
+
 
 function init(){
     rangeBar.addEventListener('input', handleRange);
+    guessNumber.addEventListener('change', getUserGuess);
 }
 
 init();
