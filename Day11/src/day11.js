a65b2a38e418c61e66e7474569ca618a33209372
ItsmeLee <arsmagica90@gmail.com>
2021-01-22 02:12:01 +0900

    Add result function
    Add new function that when user click the equal button, the result display in display box.

--- Day11/src/day11.js
+++ Day11/src/day11.js
@@ -1,15 +1,32 @@
 const keys = document.querySelectorAll("input");
 const displayContainer = document.getElementById('display');
 const resetBtn = document.getElementById("clear");
+const operators = document.querySelectorAll(".operator");
+const equalBtn = document.getElementById("equal");
 
 function getValue(event){
     const currentValue = event.target.value;
-    displayContainer.value += currentValue;
-    return currentValue;
+    displayValue(currentValue);
 }
 
 function resetDisplay(){
     displayContainer.value = "";
+}
+
+function displayValue(number){
+    displayContainer.value += number;
+}
+
+function checkState(event){
+    const currentOperator = event.target.value;
+    displayValue(currentOperator);
+}
+
+function getResult(){
+    const formula = displayContainer.value;
+    const result = eval(formula);
+    resetDisplay();
+    displayValue(result);
 }
 
 function init(){
@@ -17,6 +34,10 @@ function init(){
         key.addEventListener("click", getValue);
     });
     resetBtn.addEventListener("click", resetDisplay);
+    operators.forEach(operator => {
+        operator.addEventListener("click", checkState);
+    });
+    equalBtn.addEventListener('click', getResult);
 }
 
 init();
