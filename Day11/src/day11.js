77e04cfd55855cd3c3af9e76718206bfa82910f1
ItsmeLee <arsmagica90@gmail.com>
2021-01-22 00:49:00 +0900

    Reset Display function
    Add new function that reset the display.
    when user click the clear button, the display value is reset.

--- Day11/src/day11.js
+++ Day11/src/day11.js
@@ -1,5 +1,6 @@
 const keys = document.querySelectorAll("input");
 const displayContainer = document.getElementById('display');
+const resetBtn = document.getElementById("clear");
 
 function getValue(event){
     const currentValue = event.target.value;
@@ -7,10 +8,15 @@ function getValue(event){
     return currentValue;
 }
 
+function resetDisplay(){
+    displayContainer.value = "";
+}
+
 function init(){
     keys.forEach(key => {
         key.addEventListener("click", getValue);
     });
+    resetBtn.addEventListener("click", resetDisplay);
 }
 
 init();
