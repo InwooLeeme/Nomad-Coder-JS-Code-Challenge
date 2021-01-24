0dd1611fa630e87af452b9b470dcf6635052d700
ItsmeLee <arsmagica90@gmail.com>
2021-01-24 17:05:04 +0900

    Add delete to do function
    Add delete to do function in todo.js file.
    When user click the delete button in each to do list, the clicked to do list delete immediately.

--- Momentum/src/todo.js
+++ Momentum/src/todo.js
@@ -2,7 +2,8 @@ const toDoForm = document.querySelector(".toDoForm");
 const toDoInput = document.querySelector(".toDoInput");
 const toDoList = document.querySelector(".toDoList");
 
-const toDoArray = [];
+let toDoArray = [];
+const TODOLS = "ToDos";
 
 function handleSubmit(event){
     event.preventDefault();
@@ -11,18 +12,36 @@ function handleSubmit(event){
     addToDos(currentToDos);
 }
 
+function saveToDos(toDoArray){
+    localStorage.setItem(TODOLS, toDoArray);
+}
+
+function deleteToDos(event){
+    const li = event.path[1];
+    toDoList.removeChild(li);
+    deleteToDosInLs(li);
+}
+
+function deleteToDosInLs(li){
+    let cleanToDos = toDoArray.filter((toDo) => {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDoArray = cleanToDos;
+    saveToDos(JSON.stringify(toDoArray));
+}
+
 function addToDos(toDos){
     const li = document.createElement("li");
     const checkBox = document.createElement("input");
     const span = document.createElement("span");
     const button = document.createElement("button");
-    
     span.innerHTML = toDos;
     button.innerHTML = "❌";
+    button.addEventListener("click", deleteToDos);
     checkBox.type = "checkbox"; 
     checkBox.name = "name"; 
     checkBox.value = "value"; 
-    checkBox.id = toDoArray.length + 1;
+    /* checkBox.id = toDoArray.length + 1; */
     toDoList.appendChild(li);
     li.appendChild(checkBox);
     li.appendChild(span);
@@ -31,12 +50,25 @@ function addToDos(toDos){
         id: toDoArray.length + 1,
         text : toDos,
     }
+    li.id = toDoObj.id;
     toDoArray.push(toDoObj);
-    console.table(toDoArray);
+    saveToDos(JSON.stringify(toDoArray));
+}
+
+function loadToDos(){
+    const getToDos = localStorage.getItem(TODOLS);
+    if(getToDos !== null){
+        // toDoArray is not null
+        const parseToDos = JSON.parse(getToDos);
+        parseToDos.forEach((toDo) =>{
+            addToDos(toDo.text);
+        });
+    }
 }
 
 function controller(){
     toDoForm.addEventListener('submit', handleSubmit);
+    loadToDos();
 }
 
 controller();
